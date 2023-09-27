5e6cad5bd299d847fa4d820ccd9b348d750befa0
faithwest <faithchepkoech425@gmail.com>
2023-09-27 15:58:43 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+//removing the main using JavaScript and DOM
+main.remove();
+
+//Adding newHeader variable
+let newHeader = document.createElement("h1");
+
+//giving the newHeader id
+newHeader.id = "victory";
+
+//newHeader content 
+newHeader.textContent = "YOUR-NAME is the champion";
+
+//appending the newHeader to the body   
+let parentElement = document.body;
+
+parentElement.appendChild(newHeader);
 // Write your code here!
